f963bc541fe1dfcc1f418b79f52dfa69a7d1375e
Kota Ishimoto <ishikota086@gmail.com>
2015-12-06 16:41:53 +0900

    Add inverting rule 'not' on ViewMatcherInteraction

--- cappuchino/src/main/java/jp/ikota/cappuchino/matcher/ViewMatcherInteraction.java
+++ cappuchino/src/main/java/jp/ikota/cappuchino/matcher/ViewMatcherInteraction.java
@@ -3,6 +3,9 @@ package jp.ikota.cappuchino.matcher;
 
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.matcher.ViewMatchers;
+import android.view.View;
+
+import org.hamcrest.Matcher;
 
 import jp.ikota.cappuchino.matcher.custommatcher.CustomMatcher;
 
@@ -16,38 +19,49 @@ import static org.hamcrest.Matchers.anything;
 
 public class ViewMatcherInteraction {
 
-    private ViewInteraction mViewInteraction;
+    public final NotViewMatcherInteraction not;
+
+    private final ViewInteraction mViewInteraction;
+    protected boolean not_flg = false;
 
     public ViewMatcherInteraction(ViewInteraction viewInteraction) {
         mViewInteraction = viewInteraction;
+        not = new NotViewMatcherInteraction(viewInteraction);
+    }
+
+    // This constructor is called from NotViewMatcherInteraction
+    // to avoid circular execution of calling constructor.
+    public ViewMatcherInteraction(ViewInteraction viewInteraction, Object anything) {
+        mViewInteraction = viewInteraction;
+        not = null;
     }
 
     public void isVisible() {
-        mViewInteraction.check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        match((withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 
     public void isInvisible() {
-        mViewInteraction.check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+        match(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE));
     }
 
     public void isGone() {
-        mViewInteraction.check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        match(withEffectiveVisibility(ViewMatchers.Visibility.GONE));
     }
 
     public void isDisplayed() {
-        mViewInteraction.check(matches(android.support.test.espresso.matcher.ViewMatchers.isDisplayed()));
+        match(android.support.test.espresso.matcher.ViewMatchers.isDisplayed());
     }
 
     public void isDisplayedAtLeast(int areaPercentage) {
-        mViewInteraction.check(matches(isDisplayingAtLeast(areaPercentage)));
+        match(isDisplayingAtLeast(areaPercentage));
     }
 
     public void hasText(String text) {
-        mViewInteraction.check(matches(withText(text)));
+        match(withText(text));
     }
 
     public void hasText(int resourceId) {
-        mViewInteraction.check(matches(withText(resourceId)));
+        match(withText(resourceId));
     }
 
     public void exists() {
@@ -55,11 +69,29 @@ public class ViewMatcherInteraction {
     }
 
     public void listItemCountIs(int expected_item_count) {
-        mViewInteraction.check(matches(withListItemCount(expected_item_count)));
+        match(withListItemCount(expected_item_count));
     }
 
     public void should(CustomMatcher.MatcherRule rule) {
-        mViewInteraction.check(matches(withCustomMatch(rule)));
+        match(withCustomMatch(rule));
+    }
+
+    public class NotViewMatcherInteraction extends ViewMatcherInteraction {
+        @SuppressWarnings("unused")
+        private final NotViewMatcherInteraction not = null;
+
+        public NotViewMatcherInteraction(ViewInteraction viewInteraction) {
+            super(viewInteraction, new Object());
+            not_flg = true;
+        }
+    }
+
+    private void match(Matcher<? super View> matcher) {
+        if(not_flg) {
+            mViewInteraction.check(matches(org.hamcrest.Matchers.not(matcher)));
+        } else {
+            mViewInteraction.check(matches(matcher));
+        }
     }
 
 }
